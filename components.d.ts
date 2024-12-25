5f821f43354bd915f055e16309a237ed98353974
liang <2501119005@qq.com>
2024-12-25 18:04:35 +0800

    添加聊天功能

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
+    ElCol: typeof import('element-plus/es')['ElCol']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
--- src/views/home/components/chatRoom/index.vue
+++ src/views/home/components/chatRoom/index.vue
@@ -1,100 +1,194 @@
 <template>
   <div class="chat-room">
     <div class="message-list">
-      <div v-for="(message, index) in messages" :key="index" class="message">
+      <!-- <div v-for="(message, index) in messages" :key="index" class="message">
         <strong>{{ message.sender }}:</strong>
         <span>{{ message.content }}</span>
-      </div>
+      </div> -->
+
+      <template v-for="(item, index) in messages" :key="index">
+        <div class="list-row" v-if="item.type === 'message'" :class="{ myMassageRow: item.isMyMessage }">
+          <el-avatar class="avatar" shape="square" :size="40" fit="cover" :src="url" />
+          <div class="content">
+            <div class="name" v-if="!item.isMyMessage">{{ item.user.name }}</div>
+            <div class="massage">
+              <div class="text">{{ item.content }}</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="list-info" v-else>
+          <div class="message-info">{{ item.content }}</div>
+        </div>
+      </template>
     </div>
 
     <div class="input-container">
       <el-input v-model="newMessage" :rows="2" type="text" placeholder="Type a message" :disabled="isSending"
-        @keyup.enter="sendMessage" />
-      <el-button type="success" @click="sendMessage" :disabled="isSending">发送</el-button>
+        @keyup.enter="handleSendMessage" />
+      <el-button type="success" @click="handleSendMessage" :disabled="isSending">发送</el-button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { io } from "socket.io-client";
-import { ref, nextTick } from 'vue';
+import { ref, nextTick, onBeforeUnmount } from 'vue';
 import { useUserStore } from '@/store/user'; // 引入 Pinia Store
 import { AckResponse } from '@/types/index'
 // 定义 Message 类型
-interface Message {
-  sender: string;
+interface BaseMessage {
+  type: 'info';
+  isMyMessage?: never; // 当类型为 'info' 时，不能传 isMyMessage
+  user: {
+    name: string;
+    _id: string;
+    socketId: string;
+  };
   content: string;
 }
 
-// webSocket
-const socket = io('http://localhost:3000', {
-  // withCredentials: true,  // 如果你需要携带凭证（如 cookies）
-  transports: ['websocket'],  // 强制使用 WebSocket 协议
-});
+interface MessageWithType {
+  type: 'message';
+  isMyMessage: boolean; // 当类型为 'message' 时，必须传 isMyMessage
+  user: {
+    name: string;
+    _id: string;
+    socketId: string;
+  };
+  content: string;
+}
 
-// client-side
-socket.on("connect", () => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
-
-socket.on("disconnect", () => {
-  console.log(socket.id); // undefined
-});
+type Message = BaseMessage | MessageWithType;
+const url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 // 响应式数据
-const userName = 'User1';  // 用户名，假设为 User1
 const messages = ref<Message[]>([]);
 const newMessage = ref<string>('');
 const isSending = ref<boolean>(false);
-const messageList = ref<HTMLElement | null>(null);
-
+const messageListElement = ref<HTMLElement | null>(null);
 
 // 用户信息
 const userStore = useUserStore(); // 获取 Pinia Store
 
+const { name, _id } = userStore.userInfo ? userStore.userInfo : {}
+
+// webSocket连接
+const socket = io('http://localhost:3000', {
+  // withCredentials: true,  // 如果你需要携带凭证（如 cookies）
+  transports: ['websocket'],  // 强制使用 WebSocket 协议
+
+  auth: {
+    _id: _id,
+    name: name,
+  },
+});
+
+socket.on("connect", () => {
+
+});
+socket.on("disconnect", () => {
+
+});
+
+socket.on("addUser", (message) => {
+  const content = `${message.user.name}进入了`
+  messages.value.push({
+    type: 'info',
+    user: message.user,
+    content
+  })
+});
+socket.on("removeUser", (message) => {
+  const content = `${message.user.name}退出了`
+  messages.value.push({
+    type: 'info',
+    user: message.user,
+    content
+  })
+});
+
+socket.on("onlineUsers", (message) => {
+  console.log('在线用户数据', message);
+});
+
+// 广播事件
+socket.on('broadcastToOthers', (message) => {
+  messages.value.push({
+    type: 'message',
+    user: message.user,
+    isMyMessage: false,
+    content: message.content
+  });
+});
+
+
+
+// 封装 sendMessage 方法
+const sendMessage = <T = any>(
+  event: string,
+  message: any
+): Promise<AckResponse<T>> => {
+  return new Promise((resolve, reject) => {
+    // 发送消息并处理回调
+    socket.emit(event, message, (ack: AckResponse<T>) => {
+      if (ack.status === 'ok') {
+        resolve(ack); // 成功时 resolve
+      } else {
+        reject(new Error(ack.error || 'Unknown error')); // 失败时 reject
+      }
+    });
+  });
+};
+
 // 发送消息函数
-const sendMessage = () => {
+const handleSendMessage = () => {
   if (newMessage.value.trim() === '') return;
   if (userStore.userInfo === null) {
     // 没有用户信息
     return
   }
-  const { name } = userStore.userInfo
   isSending.value = true;
 
-
-  // 连接成功后，向服务器发送消息
-  socket.emit('message', {
-    sender: name,
+  sendMessage('message', {
+    user: { name, _id, socketId: socket.id },
     content: newMessage.value
-  }, (ack: AckResponse) => {
-    if (ack.status === 'ok') {
-      console.log('Message sent successfully:', ack.data);
-    } else {
-      console.error('Message failed to send:', ack.error);
-    }
-  });
-  setTimeout(() => {
+  }).then(res => {
     messages.value.push({
-      sender: name,
-      content: newMessage.value
+      type: 'message',
+      user: res.data.user,
+      isMyMessage: true,
+      content: res.data.content
     });
 
     newMessage.value = '';  // 清空输入框
     isSending.value = false;
-  }, 1000);  // 模拟网络延迟
+
+    toScrollButtom()
+  })
 };
 
 const toScrollButtom = () => {
   // 滚动到最新消息
   nextTick(() => {
-    if (messageList.value) {
-      messageList.value.scrollTop = messageList.value.scrollHeight;
+    if (messageListElement.value) {
+      messageListElement.value.scrollTop = messageListElement.value.scrollHeight;
     }
   });
 }
+
+
+onBeforeUnmount(() => {
+  if (socket) {
+    socket.emit('client-disconnect', {
+      _id: _id,
+      name: name,
+    });
+    socket.disconnect();
+  }
+});
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .chat-room {
   width: 100%;
   height: 100%;
@@ -113,6 +207,84 @@ const toScrollButtom = () => {
   min-height: 300px;
   overflow-y: auto;
   margin-bottom: 10px;
+
+  .list-row {
+    display: flex;
+
+    /* 整体水平翻转 */
+    .content {
+      margin-left: 10px;
+
+      .name {
+        vertical-align: top;
+        size: 12px;
+        color: #ccc;
+      }
+
+      .massage {
+        max-width: 300px;
+        margin-left: 10px;
+        background-color: #fff;
+        padding: 8px;
+        border-radius: 4px;
+        position: relative;
+        overflow-wrap: break-word;
+        /* 或者使用 word-break */
+        word-break: break-all;
+
+        /* 保持不换行 */
+        &::before {
+          position: absolute;
+          top: 10px;
+          left: -19px;
+          content: '';
+          display: block;
+          width: 0;
+          height: 0;
+          border-left: 10px solid transparent;
+          border-right: 10px solid #fff;
+          border-bottom: 10px solid transparent;
+          border-top: 10px solid transparent;
+        }
+
+        .text {}
+      }
+    }
+  }
+
+  .list-info {
+    padding: 10px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    .message-info {
+      background-color: #ddd;
+      color: #ffffff;
+      padding: 2px 8px;
+      border-radius: 4px;
+    }
+  }
+}
+
+.myMassageRow {
+  transform: scaleX(-1);
+
+  .name,
+  .text,
+  .avatar {
+    transform: scaleX(-1);
+    /* 再次翻转子元素，保持文字正常显示 */
+    text-align: right;
+  }
+
+  .massage {
+    background-color: #4caf50 !important;
+    &::before {
+      border-right: 10px solid #4caf50 !important;
+    }
+  }
 }
 
 .message {
